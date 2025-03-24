6c193089d2d1e3faeee174cfec7061ab0681233b
themightypedro <contato.phmoreira@gmail.com>
2025-03-24 20:16:37 -0300

    Add files via upload

---
+++ parOuImpar.py
@@ -0,0 +1,5 @@
+num = int(input("Entre com um número inteiro: "))
+if num % 2 == 0: #Se não for par, então é ímpar
+        print(f"O número  {num}  é par.")
+else:
+    print(f"O número  {num}   é impar.")
