16408862f39b8e47faaf56dfa4968c1fd5d66e53
themightypedro <contato.phmoreira@gmail.com>
2025-03-24 21:36:57 -0300

    Add files via upload

---
+++ raizQuadrada.py
@@ -0,0 +1,6 @@
+num = int(input("Digite um número qualquer: "))
+if (num > 0):
+    r = (num**0.5)
+    print("A raiz quadrada do número digitado é: ",r)
+else:
+    print("Não é possível calcular raiz quadrada de número negativo")
